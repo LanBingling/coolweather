375a5fcb26dc7828e5b14f57928aa855731a79ef
lanshuai <lanshuai@inke.cn>
2018-07-11 19:02:22 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/lansir/coolweather/util/ToastUtil.java
@@ -0,0 +1,20 @@
+package com.lansir.coolweather.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+    private static Toast mToast;
+    public static void showToast(Context context, int resId, int duration) {
+        showToast(context, context.getString(resId), duration);
+    }
+    public static void showToast(Context context, String msg, int duration) {
+        if (mToast == null) {
+            mToast = Toast.makeText(context, msg, duration);
+        }
+        else {
+            mToast.setText(msg);
+        }
+        mToast.show();
+    }
+}
